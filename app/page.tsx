3e07c7c74e8407387827fa857c6da3a7f0c38402
RNubla <rnubla97@gmail.com>
2024-02-28 21:24:34 -0500

    add more function for testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,27 @@
 "use client";
-import init, { add } from "wasm-lib";
+import init, { add, greet } from "wasm-lib";
 import { useEffect, useState } from "react";
 
 export default function Home() {
 	const [ans, setAns] = useState(0);
+	const [message, setMessage] = useState("");
 	useEffect(() => {
 		init().then(() => {
-			setAns(add(1, 1));
+			setMessage(greet("Hello WASM"));
 		});
 	});
+
+	const handleAdd = (input: number) => {
+		console.log(`input ${ans}`);
+		setAns((prevState) => add(prevState, input));
+	};
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<p>{message}</p>
 			<p>wasm add {ans}</p>
+			<button type="button" onClick={() => handleAdd(2)}>
+				Add
+			</button>
 		</main>
 	);
 }
